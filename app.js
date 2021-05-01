86e0ac6e24ebb3b2d74469902eb6a73c7af2c4d9
wilmerjan <wilmerjan.pascua@lorma.edu>
2021-05-01 15:40:36 +0800

    Level 3 completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +35,7 @@ app.get('/register', (req,res)=>{
 app.post('/register', (req,res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -51,7 +50,7 @@ app.post('/register', (req,res)=>{
 
 app.post('/login', (req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, foundUser)=>{
         if (err){
